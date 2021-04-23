2d0ead134fb0c993899d1676899a521302c5a2c6
Ole-Marius Johansen <ojo067@uit.no>
2021-04-23 15:51:21 +0200

    blogg objekt test

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from database import myDB
+from blogg import Blogg
 
 app = Flask(__name__, template_folder='templates')
 
@@ -8,8 +9,9 @@ app = Flask(__name__, template_folder='templates')
 def forside() -> 'html':
     with myDB() as db:
         result = db.selectBlogg()
+    bloggObjektene = [Blogg(*x) for x in result]
 
-    return render_template('index.html', blogg=result)
+    return render_template('index.html', blogg=bloggObjektene)
 
 
 if __name__ == '__main__':
---
+++ blogg.py
@@ -0,0 +1,5 @@
+class Blogg:
+
+    # construct / attributes
+    def __init__(self, blogg_navn):
+        self.blogg_navn = blogg_navn
