3c6d503f3998fa247b001e02a3bfe7f47e7d1ad0
Vegard1 <vol033@uit.no>
2021-04-23 14:58:02 +0200

    skriv ut blogg

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from database import myDB
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def forside() -> 'html':
+    with myDB() as db:
+        result = db.selectBlogg()
 
-    return render_template('index.html')
+    return render_template('index.html', blogg=result)
 
 
 if __name__ == '__main__':
