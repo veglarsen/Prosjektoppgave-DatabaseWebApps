58f775c3672ffe4fb19bd250a359c669f1aad829
Vegard Larsen <vla018@post.uit.no>
2021-04-27 11:10:12 +0200

    brukerSkjema.py done

--- brukerSkjema.py
+++ brukerSkjema.py
@@ -1,4 +1,13 @@
-from wtforms import Form
+from wtforms import Form, StringField, HiddenField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email, Length
+
 
 class BrukerSkjema(Form):
+    brukerNavn = StringField('Username', validators=[DataRequired()])
+    forNavn = StringField('First name', validators=[DataRequired()])
+    etterNavn = StringField('Last name', validators=[DataRequired()])
+    eMail = EmailField('E-mail Address', validators=[DataRequired(), Email(), Length(max=120)])
+    ID = HiddenField()
+    submit = SubmitField('Update user')
 
