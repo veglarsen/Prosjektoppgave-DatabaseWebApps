6a22684163c155412ea89e1ba04aa907eb6922c3
Vegard Larsen <vla018@post.uit.no>
2021-04-27 11:24:18 +0200

    added password

--- brukerSkjema.py
+++ brukerSkjema.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, HiddenField, SubmitField
+from wtforms import Form, StringField, HiddenField, SubmitField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, Length
 
@@ -7,6 +7,11 @@ class BrukerSkjema(Form):
     brukerNavn = StringField('Username', validators=[DataRequired()])
     forNavn = StringField('First name', validators=[DataRequired()])
     etterNavn = StringField('Last name', validators=[DataRequired()])
+    password = PasswordField('New Password', [
+        validators.InputRequired(),
+        validators.EqualTo('pwconfirm', message='Passwords must match')
+     ])
+    pwconfirm = PasswordField('Repeat Password')
     eMail = EmailField('E-mail Address', validators=[DataRequired(), Email(), Length(max=120)])
     ID = HiddenField()
     submit = SubmitField('Update user')
