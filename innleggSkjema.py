c7754c2b910ca74dc11ab12b59a6516bb1c95619
Vegard Larsen <vla018@post.uit.no>
2021-04-29 09:51:54 +0200

    done but needs tag query

--- innleggSkjema.py
+++ innleggSkjema.py
@@ -1,17 +1,18 @@
-from wtforms import Form, StringField, HiddenField, SubmitField, validators, SelectField, TextAreaField
+from wtforms import Form, StringField, HiddenField, SubmitField, SelectField, TextAreaField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Length
+from database import myDB
+
+with myDB() as db:
+    selectTag = db.selectTag()
 
 
 class InnleggSkjema(Form):
     tittel = StringField('Title: ', validators=[DataRequired(), Length(max=20)])
     ingress = StringField('Preface text: ', validators=[DataRequired(), Length(max=50)])
-    innlegg = TextAreaField('Body text: ', validators=[DataRequired(), Length(max=6000)])
+    innlegg = TextAreaField('Body text: ', validators=[DataRequired(), Length(max=6000)])  # cirka 2 sider
     tag = SelectField(u'Tag: ', choices=selectTag)
     dato = DateField('Dato', validators=[DataRequired()])
-
-    treff = StringField('First name: ', validators=[DataRequired()])
-    innleggID = HiddenField()
+    # innleggID = HiddenField() # tror ikke denne er nødvendig
     bloggID = HiddenField()
     submit = SubmitField('Submit form')
-
