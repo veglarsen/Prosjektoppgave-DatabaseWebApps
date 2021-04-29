b1b541446e2ee3f971e6a3dcfa74b879eeb7f716
Vegard Larsen <vla018@post.uit.no>
2021-04-29 09:32:51 +0200

    forgot length on brukerSkjema.py

--- innleggSkjema.py
+++ innleggSkjema.py
@@ -1,13 +1,13 @@
-from wtforms import Form, StringField, HiddenField, SubmitField, validators, SelectField
+from wtforms import Form, StringField, HiddenField, SubmitField, validators, SelectField, TextAreaField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Email, Length
 
 
 class InnleggSkjema(Form):
-    tittel = StringField('Title: ', validators=[DataRequired()])
-    ingress = StringField('Preface text: ', validators=[DataRequired()])
-    innlegg = TextAreaField(' ', validators=[DataRequired(), Length(max=250)])
-    tag = SelectField(u'kategorier', choices=selectTag)
+    tittel = StringField('Title: ', validators=[DataRequired(), Length(max=20)])
+    ingress = StringField('Preface text: ', validators=[DataRequired(), Length(max=50)])
+    innlegg = TextAreaField('Body text: ', validators=[DataRequired(), Length(max=6000)])
+    tag = SelectField(u'Tag: ', choices=selectTag)
     dato = DateField('Dato', validators=[DataRequired()])
 
     treff = StringField('First name: ', validators=[DataRequired()])
