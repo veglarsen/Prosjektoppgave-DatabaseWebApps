61da6da999cb2f36ac0b099189d1071f636a2ba4
Vegard Larsen <vla018@post.uit.no>
2021-04-29 09:20:26 +0200

    added fields from db

--- innleggSkjema.py
+++ innleggSkjema.py
@@ -1,16 +1,16 @@
-from wtforms import Form, StringField, HiddenField, SubmitField, PasswordField, validators
-from wtforms.fields.html5 import EmailField
+from wtforms import Form, StringField, HiddenField, SubmitField, validators, SelectField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Email, Length
 
 
 class InnleggSkjema(Form):
-    tittel = StringField('First name: ', validators=[DataRequired()])
-    ingress = StringField('First name: ', validators=[DataRequired()])
-    treff = StringField('First name: ', validators=[DataRequired()])
-    tag = StringField('First name: ', validators=[DataRequired()])
-    dato = StringField('First name: ', validators=[DataRequired()])
+    tittel = StringField('Title: ', validators=[DataRequired()])
+    ingress = StringField('Preface text: ', validators=[DataRequired()])
+    innlegg = TextAreaField(' ', validators=[DataRequired(), Length(max=250)])
+    tag = SelectField(u'kategorier', choices=selectTag)
+    dato = DateField('Dato', validators=[DataRequired()])
 
-    innlegg = StringField('Last name: ', validators=[DataRequired()])
+    treff = StringField('First name: ', validators=[DataRequired()])
     innleggID = HiddenField()
     bloggID = HiddenField()
     submit = SubmitField('Submit form')
